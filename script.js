3160550089ebbf5de1e13997f61db9047aaa1607
YuliiaIIIII <yulyamashka@gmail.com>
2023-04-04 16:46:20 +0200

    added nav letters

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let transform = document.querySelector('.userPhoto');
 
 function scaleFn(){
     transform.classList.add('transformPhotoSize');
-
+    document.querySelector('.userPhoto').innerHTML = ("<img onclick='cancelScale()' src='self-photo.jpg' alt='Profile photo Y.Malykhina'>");
+}
+function cancelScale(){
+    transform.classList.remove('transformPhotoSize');
 }
 
-console.log(transform);
 
 
 // transform.addEventListener('click',(event) => {

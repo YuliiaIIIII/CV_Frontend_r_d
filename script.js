46af4016b6a63463fa184e24712d3edc3ea6de93
YuliiaIIIII <yulyamashka@gmail.com>
2023-04-06 17:45:13 +0200

    added navigation using keyboard keys

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let transform = document.querySelector('.userPhoto');
 
+transform.addEventListener('click',() => {
+	transform.classList.toggle('transformPhotoSize');
+});
 
-function scaleFn(){
-    transform.classList.add('transformPhotoSize');
-    document.querySelector('.userPhoto').innerHTML = ("<img onclick='cancelScale()' src='self-photo.jpg' alt='Profile photo Y.Malykhina'>");
-}
-function cancelScale(){
-    transform.classList.remove('transformPhotoSize');
-}
-
-
-
-// transform.addEventListener('click',(event) => {
-
-// });
+document.addEventListener('keydown', function (event){ 
+    if (event.key == 's' || event.key == 'S' ) {
+        const element = document.querySelector("#letterS");
+        element.scrollIntoView(true);
+    }
+    else if (event.key == 'h' || event.key == 'H' ) {
+        const element = document.querySelector("#letterH");
+        element.scrollIntoView(true);
+    }
+    else if (event.key == 'w' || event.key == 'W' ) {
+        const element = document.querySelector("#letterW");
+        element.scrollIntoView(true);
+    }
+    else if (event.key == 'e' || event.key == 'E' ) {
+        const element = document.querySelector("#letterE");
+        element.scrollIntoView(true);
+    }
+});
